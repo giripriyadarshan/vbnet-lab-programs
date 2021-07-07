12fa91874896bdc7ba12918d59eccfda325e0e1f
Giri Priyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2021-07-07 13:04:17 +0530

    slight changes

--- Lab-Programs/Part-B-Programs/Program17.vb
+++ Lab-Programs/Part-B-Programs/Program17.vb
@@ -1,12 +1,10 @@
 ﻿Public Class Program17
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        If RadioButton1.Checked = True Then
+        If RadioButton1.Checked Then
             TextBox1.Text = "Stop dont move.."
-        End If
-        If RadioButton2.Checked = True Then
+        ElseIf RadioButton2.Checked Then
             TextBox1.Text = "You are Ready to move.."
-        End If
-        If RadioButton3.Checked = True Then
+        ElseIf RadioButton3.Checked Then
             TextBox1.Text = "Go start moving.."
         End If
     End Sub
